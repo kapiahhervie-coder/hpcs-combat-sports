cd63fd7a508cad70cf63109b9e655a7bc2bcba2a
kapiahhervie-coder <kapiah.hervie@gmail.com>
2026-07-05 20:31:52 +0800

    Security: pindahkan SECRET_KEY dan EMAIL_HOST_PASSWORD ke environment variable (.env)

--- hpcs_config/settings.py
+++ hpcs_config/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!(5nyyqqgn84l(vr7=l^1gi9(^xfqy_68=$z8228oo*gsgl8('
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -138,7 +139,7 @@ EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'sakalahpcs@gmail.com'
-EMAIL_HOST_PASSWORD = 'evsz uojq ckud hans'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'HPCS Combat Sports <sakalahpcs@gmail.com>'
 
 
