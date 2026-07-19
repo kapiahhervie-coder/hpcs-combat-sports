bc1a08b38d8e6f96bcb48905b88b820123cdbff8
kapiahhervie-coder <kapiah.hervie@gmail.com>
2026-07-19 13:32:49 +0800

    Aktifkan kembali ApprovalRequiredMiddleware setelah verifikasi

--- hpcs_config/settings.py
+++ hpcs_config/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'combat.middleware.ApprovalRequiredMiddleware',  # sementara dinonaktifkan untuk debug
+    'combat.middleware.ApprovalRequiredMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -170,6 +170,7 @@ DEFAULT_FROM_EMAIL = 'HPCS Combat Sports <sakalahpcs@gmail.com>'
 # Allow GET logout
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/combat/'
+
 
 
 
